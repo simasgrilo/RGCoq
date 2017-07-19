2af08a37dfe2b11ca05ca579694ecaaac9ff50be
simasgrilo <simasgrilo86@gmail.com>
2017-07-19 01:32:31 -0300

    Correction of some proofs

--- soundness.v
+++ soundness.v
@@ -25,6 +25,14 @@ Section rhs_lemmas.
   - intros. rewrite H. reflexivity.
   Qed.
 
+  Lemma isEmpty_false : forall NT T, forall r:rhs.t T NT, rhs.isEmpty r = false <-> (r <> Empty).
+  Proof.
+  intros.
+  rewrite <- isEmpty_true.
+  rewrite not_true_iff_false.
+  reflexivity.
+  Qed.
+
 End rhs_lemmas.
 
 (*Lemmas about the soundness of functions within the reg_grammar module        *)
@@ -51,16 +59,30 @@ Section reg_grammar_lemmas.
   right. right. exists (n). reflexivity.
   Qed.
 
-  (*hold on:*)
+   (*hold on*)(*
+  Definition getRHS `{EqDec NT eq}
+           (nt : NT) : list (NT * rhs.t T NT) ->
+                       list (rhs.t T NT) :=
+    filterMap (fun rule => let '(nt', rhs) := rule in
+                        if nt == nt' then Some rhs else None). *)(*
   Lemma getRHS_sound : forall nt, forall rules, reg_grammar.getRHS nt rules = [] \/
-  (exists r:rhs.t T NT,exists a: (NT * rhs.t T NT),In (r) (reg_grammar.getRHS nt rules)
-  /\ r = snd a /\ fst a = nt).
-  Proof.
+  (exists a: (NT * rhs.t T NT),exists n:NT,In (snd a) (reg_grammar.getRHS (nt) rules) /\ n = fst a).
   intros. destruct rules.
-  - left. simpl. reflexivity.
-  - right. exists (snd p). exists p. split.
-    + simpl. case p. intros. destruct equiv_dec. simpl. left. reflexivity.
-     Abort.
+  Proof.
+  - left. reflexivity.
+  - right. exists p. exists nt. 
+  Lemma getRHS_sound : forall nt, forall rules, reg_grammar.getRHS nt rules = [] \/
+  (exists n:(NT),exists rhs: rhs.t T NT, exists rule: NT * rhs.t T NT,
+  In (rhs) (reg_grammar.getRHS (nt) rules) /\ fst rule = nt /\ snd rule = rhs).
+  Proof.
+  intros.
+  destruct rules.
+  - left. reflexivity.
+  - right. exists nt. exists (snd p). exists p.
+    simpl.
+    destruct equiv_dec with (x:=fst p)(y:= nt).
+    rewrite e.  
+  Admitted.*)
 
   (*The following lemma states the soundness of the step_nt function. *)
   Lemma step_nt_sound : forall rules,forall t, forall nt, reg_grammar.step_nt rules t nt =  [] \/
@@ -123,19 +145,61 @@ Section reg_grammar_lemmas.
     destruct reg_grammar.is_final;auto.
   Qed.
 
-  (*As defined by is_final's behaviour, it should return true iff there is a None *)
-  (* in the list of nonterminal symbols, meaning it reached a valid rule which is from the *)
-  (* kind A -> "a" or A -> e, "a" being a terminal character and e means the empty string char. *)
-  Lemma is_final_true : forall r:list (NT * rhs.t T NT),forall a:list (option NT), exists l,
-  reg_grammar.is_final r l = true <-> In (None) l.
+  (* -- In (None) a prob. *)(*
+  Inductive final_state (g:reg_grammar.g T NT)  : (list (option NT)) -> Prop :=
+  | state_final_e :
+                  (reg_grammar.is_final (reg_grammar.rules g) [] = true) -> final_state g []
+
+  | state_final :  forall a, (In (None) a \/
+                  (exists n,(In (Some n) a /\ reg_grammar.is_final (reg_grammar.rules g) (a) = true))) -> final_state g a.
+
+  (*testing with inductive proposition*)
+  Lemma is_final_true_inductive : forall l,forall g:reg_grammar.g T NT,
+  reg_grammar.is_final (reg_grammar.rules g) l = true <-> final_state g l.
   Proof.
   intros.
-  exists (None::a).
   split.
-  - intros. simpl. left. reflexivity. 
-  - intros. simpl. reflexivity.
+  - intros. destruct l. inversion H1. apply state_final with (g:= g).
+    destruct o. right. exists n. split. 
+        + simpl. left. reflexivity.
+        + assumption. 
+        + left. simpl. left. reflexivity.
+  - intros. destruct H1.
+    + assumption.
+    + destruct H1 as [left | right]. destruct a. inversion left. inversion left. rewrite H1. reflexivity.
+    (*stuck*) *)
+
+  (*As defined by is_final's behaviour, it should return true if there is a None *)
+  (* in the list of nonterminal symbols, meaning it reached a valid rule which is from the *)
+  (* kind A -> "a" or A -> e, "a" being a terminal character and e means the empty string char. *)
+  Lemma is_final_true : forall g:reg_grammar.g T NT, forall l: list (option NT),
+  reg_grammar.is_final (reg_grammar.rules g) l = true -> (reg_grammar.is_final (reg_grammar.rules g) [] = true 
+  \/  (In (None) (l) \/ exists n,(In (Some n) l /\ reg_grammar.is_final (reg_grammar.rules g) (l) = true))).
+  Proof.
+  intros.
+  - intros. destruct l. left. assumption.
+    right. destruct o. right. exists n. split.
+    + simpl; auto.
+    + assumption.
+    + left. simpl. auto.
   Qed.
 
+  (* -- (In (None) (l) prob; same with inductive proposition.
+   Lemma is_final_true_forall : forall g:reg_grammar.g T NT, forall l: list (option NT),
+  reg_grammar.is_final (reg_grammar.rules g) l = true <-> (reg_grammar.is_final (reg_grammar.rules g) l = true 
+  \/  (In (None) (l) \/ exists n,(In (Some n) l /\ reg_grammar.is_final (reg_grammar.rules g) (l) = true)))).
+  Proof.
+  intros.
+  split.
+  - intros. destruct l. left. assumption.
+    right. destruct o. right. exists n. split.
+    + simpl; auto.
+    + assumption.
+    + left. simpl. auto.
+    - intros. destruct l. destruct H1 as [left | right]. assumption. 
+      + destruct right as [left' | right']. inversion left'. destruct right'. destruct H1. inversion H1.
+      + destruct H1 as [left | right]. assumption. destruct right as [left' | right']. destruct o.
+      rewrite H1. reflexivity. Admitted. *)
 
   (*The following lemmas states properties about the soundness of the grammar parser *)
   Lemma parse_companion : forall grammar, forall l, reg_grammar.parse grammar l = true \/ 
@@ -145,13 +209,11 @@ Section reg_grammar_lemmas.
   destruct reg_grammar.parse;auto.
   Qed.
 
-
-
   (*a word is recognizable by the parser if it can be generated by the grammar's rules *)
   (*In this sense, the parser returns true iff exists an final state in the grammar, which *)
   (*is reachable by applying parse' in the list of terminals, starting in the start symbol *)
-  (*of the grammar                                                                       *)
-  Lemma parse_returns_true : forall grammar, forall l: list T,exists s,
+  (*of the grammar. incomplete lemma.                                                      *)
+  (* Lemma parse_returns_true : forall grammar, forall l: list T,exists s,
   reg_grammar.parse grammar l = true <-> 
   reg_grammar.is_final (reg_grammar.rules grammar) s = true.
   Proof.
@@ -161,7 +223,43 @@ Section reg_grammar_lemmas.
   split.
   - intros. rewrite <- H1. reflexivity.
   - intros. symmetry. rewrite <- H1. reflexivity.
+  Qed. *)
+
+  (*this inductive proposition states when the parser for a given grammar in a list of *)
+  (*terminal symbols should return true. This comes straight from parse's definition. *)
+  Inductive parse_true (g: reg_grammar.g T NT) : list T -> Prop :=
+  | parse_t : forall l: list T,
+                 ([Some (reg_grammar.start_symbol g)] |> reg_grammar.parse' (reg_grammar.rules g) l |> 
+                  reg_grammar.is_final (reg_grammar.rules g) = true) -> parse_true g l.
+
+  (*This lemma states that for all list of terminal symbols, the parse on those lists will return true iff it *)
+  (* is in agreement with the conditions where the parser should return true, stated in parse_true. *)
+  Lemma parse_returns_true_forall : forall grammar, forall l: list T,
+  reg_grammar.parse grammar l = true <-> parse_true grammar l.
+  Proof.
+  intros.
+  split.
+  - intros. apply parse_t. rewrite <- H1. unfold reg_grammar.parse. reflexivity.
+  - intros. destruct H1. rewrite <- H1. reflexivity.
   Qed.
+
+  (*following the idea presented above, we can define the following proposition as the proposition *)
+  (*that states when the parser should return false:                                               *)
+  Inductive parse_false (g: reg_grammar.g T NT) : list T -> Prop :=
+  | parse_f : forall l,
+                 ([Some (reg_grammar.start_symbol g)] |> reg_grammar.parse' (reg_grammar.rules g) l |> 
+                  reg_grammar.is_final (reg_grammar.rules g) = false) -> parse_false g l.
+
+  (*Now, the lemma that states the parser should return false iff it meets the requirements to return false: *)
+  Lemma parse_returns_false_forall : forall grammar, forall l: list T,
+  reg_grammar.parse grammar l = false <-> parse_false grammar l.
+  Proof.
+  intros.
+  split.
+  - intros. apply parse_f. rewrite <- H1. unfold reg_grammar.parse. reflexivity.
+  - intros. destruct H1. rewrite <- H1. reflexivity.
+  Qed.
+
 
 End reg_grammar_lemmas.
 
@@ -177,9 +275,12 @@ Section dfa_lemmas.
     destruct dfa.run;auto.
     Qed.
 
-  (* The following lemma asserts that a run in a list of terminal symbols with a automata *)
+
+  (* The following lemma asserts that a run in a list of terminal symbols with a automata   *)
   (* shall return true iff there is an final state that is reachable from the initial state.*)
-  Lemma run_soundness :forall grammar:reg_grammar.g T NT,forall l:list T, exists final,
+  (* incomplete lemma: we prove for a specific case.                                        *)
+
+  (*Lemma run_soundness :forall grammar:reg_grammar.g T NT,forall l:list T, exists final,
   dfa.run (powerset_construction.dfa g) l = true 
   <-> dfa.is_final (powerset_construction.dfa g) final = true.
   Proof.
@@ -187,8 +288,53 @@ Section dfa_lemmas.
   exists (dfa.run' (dfa.next (powerset_construction.dfa g))
   l  (dfa.initial_state (powerset_construction.dfa g ))).
   split.
-  - intros. rewrite <- H1. simpl. simpl in H1.  reflexivity. 
+  - intros. rewrite <- H1. simpl. simpl in H1. reflexivity. 
   - intros. rewrite <- H1. reflexivity.
+  Qed. *)
+
+  (* the following inductive proposition states that, after running a word in a automata built from a grammar, *)
+  (* if it reaches a final state, then we have the run of a given list of terminals on the automata is valid   *)
+  (* returns true                                                                                              *)
+  Inductive run_true (g: reg_grammar.g T NT) : list T -> Prop := 
+  |run_t : forall l:list T, 
+    (dfa.is_final (powerset_construction.dfa g)
+    (dfa.run' (dfa.next (powerset_construction.dfa g)) l
+     (dfa.initial_state (powerset_construction.dfa g))) = true)-> run_true g l.
+
+  (* The following lemma states that, for all runs on list of terminals for all automata built from a given grammar*)
+  (* it retuns true iff it reaches a final state after running the word on the automata, starting in the initial   *)
+  (*state of the automata, as stated in "run_true".                                                                 *)
+  Lemma run_soundness_true_forall: forall grammar:reg_grammar.g T NT, forall l:list T,
+  dfa.run (powerset_construction.dfa g) l = true  <-> run_true g l.
+  Proof.
+  intros.
+  split.
+  - intros. apply run_t. rewrite <- H1. reflexivity.
+  - intros. destruct H1. rewrite <- H1. reflexivity.
   Qed.
+
+  (*Following the idea presented above, we can define the soundness in the case the automata *)
+  (*should return false after a run in a given list of terminal symbols:                     *)
+  Inductive run_false (g: reg_grammar.g T NT) : list T -> Prop := 
+  |run_f : forall l:list T, 
+    (dfa.is_final (powerset_construction.dfa g)
+    (dfa.run' (dfa.next (powerset_construction.dfa g)) l
+     (dfa.initial_state (powerset_construction.dfa g))) = false)-> run_false g l.
+
+  Lemma run_soundness_false_forall: forall grammar:reg_grammar.g T NT, forall l:list T,
+  dfa.run (powerset_construction.dfa g) l = false  <-> run_false g l.
+  Proof.
+  intros.
+  split.
+  - intros. apply run_f. rewrite <- H1. reflexivity.
+  - intros. destruct H1. rewrite <- H1. reflexivity.
+  Qed.
+  (* Then, we can conclude that both the parser for a given grammar and the automata built from *)
+  (* the same grammar does the same work. This is possible thanks to the way the automata's     *)
+  (* behaviour is defined from a grammar.                                                       *)
+  Lemma dfa_and_parser : forall l, reg_grammar.parse g l = dfa.run (powerset_construction.dfa g) l.
+    Proof.
+      reflexivity.
+    Qed.
 
 End dfa_lemmas.
